d8e82de8ecfafcc4ab5cee82308ae63689ae651f
yangziteng <1620842362@qq.com>
2022-07-13 15:00:51 +0800

    init

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-6gtqj1v4873bad50","alias":"cloud1"},{"envId":"mall-7gi19fir46652cb4","alias":"mall"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
